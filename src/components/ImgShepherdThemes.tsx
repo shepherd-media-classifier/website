6bc1bf61e7e2dd0f3125b95f435b74a4d02a7705
Ros McMahon <rosmcmahon@gmail.com>
2024-12-24 14:52:18 +0000

    style: auto theme switch Shepherd_logo

--- src/components/ImgShepherdThemes.tsx
+++ src/components/ImgShepherdThemes.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 export const ImgShepherdThemed = () => (
   <picture>
+    <source
+      srcSet={new URL('../assets/Shepherd_logo_light.svg', import.meta.url).href}
+      media="(prefers-color-scheme: light)"
+    />
     <img
       src={new URL("../assets/Shepherd_logo.svg", import.meta.url).href}
       alt="shepherd logo"
